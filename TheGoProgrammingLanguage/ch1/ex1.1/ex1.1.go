7fdd07374afc905461836f17c0851ac6ee79e41e
igor <i.matukin@gmail.com>
2018-02-11 02:39:10 +0300

    ex1.1

---
+++ TheGoProgrammingLanguage/ch1/ex1.1/ex1.1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var s, sep string
+	for i := 0; i < len(os.Args); i++ {
+		s += sep + os.Args[i]
+		sep = " "
+	}
+	fmt.Println(s)
+	s = ""
+	for _, arg := range os.Args[0:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println(s)
+	fmt.Println(strings.Join(os.Args[0:], " "))
+}
