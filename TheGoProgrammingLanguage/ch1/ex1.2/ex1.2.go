9984b46d77f5028cfc723881e4809d449482e43b
igor <i.matukin@gmail.com>
2018-02-11 03:24:05 +0300

    ex1.2

---
+++ TheGoProgrammingLanguage/ch1/ex1.2/ex1.2.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	var index = 0
+
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Println(index, os.Args[i])
+		index++
+	}
+
+	for index, arg := range os.Args[1:] {
+		fmt.Println(index, arg)
+	}
+
+	index = 0
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Println(index, strings.Join(os.Args[i:i+1], "\n"))
+		index++
+	}
+}
