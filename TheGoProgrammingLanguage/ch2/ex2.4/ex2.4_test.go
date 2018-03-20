694447b35976c6bd411b4d089e411a54b75bcaed
igor <i.matukin@gmail.com>
2018-03-20 23:49:57 +0300

    ex2.4

---
+++ TheGoProgrammingLanguage/ch2/ex2.4/ex2.4_test.go
@@ -0,0 +1,44 @@
+// Упражнение 2.4.
+// Напишите версию PopCount, которая подсчитывает биты с помощью сдвига аргумента по всем 64 позициям, проверяя при
+// каждом сдвиге крайний справа бит. Сравните производительность этой версии с выборкой из таблицы.
+
+package popcount
+
+import (
+	"testing"
+)
+
+// -- Benchmarks --
+
+func BenchmarkPopCount(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCount(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountLoop(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountLoop(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByShifting(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByShifting(0x1234567890ABCDEF)
+	}
+}
+
+func BenchmarkPopCountByClearing(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		PopCountByClearing(0x1234567890ABCDEF)
+	}
+}
+
+// goos: darwin
+// goarch: amd64
+// pkg: go/TheGoProgrammingLanguage/ch2/ex2.4
+// 2000000000	         0.47 ns/op
+// 50000000	        30.9 ns/op
+// 5000000	       386 ns/op
+// 50000000	        41.5 ns/op
+// PASS
